4a87671ebcf41945ea037b5c52b23fde65f005b1
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-06-17 22:04:31 +0100

    change method

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,23 +1,27 @@
 const path = require('path');
-const {downloadVideo}=require("../utils/download");
-const {v4 : uuidv4} = require('uuid');
+const { downloadVideo } = require("../utils/download");
+const { v4: uuidv4 } = require('uuid');
 
 
 
 async function convertVideo(req, res) {
-const url = req.body.url;
-const protocol = req.protocol;
-const hostname = req.get('host')
-const fullUrl = `${protocol}://${hostname}`;
-
-const id= uuidv4();
-const downloadPath = path.resolve(`./public/assets/video/${id}.mp4`)
   try {
-    setTimeout(async () => {
-    const video= await downloadVideo(url,downloadPath,fullUrl);
-    return res.status(200).download(video);
-    }, 15000)
-  
+    const url = req.body.url;
+    const protocol = req.protocol;
+    const hostname = req.get('host')
+    const fullUrl = `${protocol}://${hostname}`;
+
+    const id = uuidv4();
+    const downloadPath = path.resolve(`./public/assets/video/${id}.mp4`)
+    const video = await downloadVideo(url, downloadPath, fullUrl);
+
+    if (video) {
+      setTimeout(async () => {
+        return res.status(200).download(video);
+      }, 15000)
+    } else {
+      res.sendStatus(404);
+    }
   } catch (error) {
     console.error('error of: ', error);
     return res.status(500).send('Something went wrong');
@@ -25,6 +29,6 @@ const downloadPath = path.resolve(`./public/assets/video/${id}.mp4`)
 }
 
 
-module.exports = { 
-  convertVideo 
+module.exports = {
+  convertVideo
 }
